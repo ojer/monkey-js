f19eca9d2901fa65a064b2d15e96916b0718fbaa
ojer <sonamu@foxmail.com>
2023-10-07 10:47:06 +0800

    Update hideImg.user.js

--- hideImg.user.js
+++ hideImg.user.js
@@ -2,11 +2,13 @@
 // @name        HideImg
 // @namespace   ojer
 // @match       *://*/*
-// @version     1.0
+// @version     1.1
 // @author      ojer
 // @description 隐藏图片
+// @description 1.1 隐藏背景图片
 // @grant       GM_getValue
 // @grant       GM_setValue
+// @grant       GM_addStyle
 // @grant       GM_registerMenuCommand
 // @grant       GM_unregisterMenuCommand
 // @grant       GM_addValueChangeListener
@@ -15,14 +17,19 @@
 
 const { register } = VM.shortcut
 
-const styleHide = 'visibility: hidden !important;'
+const HI_STATUS = 'HI_STATUS'
+const HI_STYLE_ID = 'HI_STYLE_ID'
 
-const dataKey = 'HI_IS_HIDE'
 const captionHideKey = 'HideImg (Ctrl+Shift+I)'
 const captionShowKey = 'ShowImg (Ctrl+Shift+I)'
+const styleHide = 'visibility: hidden !important;'
+const styleBkImgHIde = '* {background-image: url("data:,") !important; }'
 
 const hide = () => {
   regMenu(true)
+  GM_addStyle(styleBkImgHIde).then((el) => {
+    GM_setValue(HI_STYLE_ID, el.getAttribute('id'))
+  })
   document.querySelectorAll('img,video').forEach((item) => {
     item.style = item.getAttribute('style') + ';' + styleHide
   })
@@ -30,16 +37,19 @@ const hide = () => {
 
 const show = () => {
   regMenu(false)
+  try {
+    document.getElementById(GM_getValue(HI_STYLE_ID)).remove()
+  } catch (ignore) {}
   document.querySelectorAll('img,video').forEach((item) => {
-    let style = item.getAttribute('style')
+    const style = item.getAttribute('style')
     if (style) {
       item.style = style.replaceAll(styleHide, '')
     }
   })
 }
 
-const listenerId = GM_addValueChangeListener(dataKey, (name, oldValue, newValue, remote) => {
-  console.log(oldValue, newValue)
+// const listenerId =
+GM_addValueChangeListener(HI_STATUS, (name, oldValue, newValue, remote) => {
   if (oldValue && !newValue) {
     show()
   } else if (!oldValue && newValue) {
@@ -49,19 +59,19 @@ const listenerId = GM_addValueChangeListener(dataKey, (name, oldValue, newValue,
 const regMenu = (isShow) => {
   GM_unregisterMenuCommand(isShow ? captionHideKey : captionShowKey)
   GM_registerMenuCommand(isShow ? captionShowKey : captionHideKey, () => {
-    GM_setValue(dataKey, !isShow)
+    GM_setValue(HI_STATUS, !isShow)
   })
 }
 
 register('c-s-i', () => {
-  GM_setValue(dataKey, !GM_getValue(dataKey))
+  GM_setValue(HI_STATUS, !GM_getValue(HI_STATUS))
 })
 
 const main = () => {
-  if (GM_getValue(dataKey, undefined) === undefined) {
-    GM_setValue(dataKey, true)
+  if (GM_getValue(HI_STATUS, undefined) === undefined) {
+    GM_setValue(HI_STATUS, true)
   }
-  if (GM_getValue(dataKey)) {
+  if (GM_getValue(HI_STATUS)) {
     hide()
   } else {
     show()
