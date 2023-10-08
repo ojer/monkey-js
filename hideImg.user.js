570956e64f7af7f27aa18221a883a569b77f5517
ojer <sonamu@foxmail.com>
2023-10-08 14:58:31 +0800

    update hideImg.js

--- hideImg.user.js
+++ hideImg.user.js
@@ -2,69 +2,82 @@
 // @name        HideImg
 // @namespace   ojer
 // @match       *://*/*
-// @version     1.2
+// @version     1.3
 // @author      ojer
 // @description 隐藏图片
 // @grant       GM_getValue
 // @grant       GM_setValue
-// @grant       GM_addStyle
+// @grant       GM_addElement
 // @grant       GM_registerMenuCommand
 // @grant       GM_unregisterMenuCommand
 // @grant       GM_addValueChangeListener
 // @require     https://unpkg.com/@violentmonkey/shortcut@1.4.1/dist/index.js
 // ==/UserScript==
 
+let hImg = undefined
 const { register } = VM.shortcut
-
 const HI_STATUS = 'HI_STATUS'
 
-const captionHideKey = 'HideImg (Ctrl+Shift+I)'
-const captionShowKey = 'ShowImg (Ctrl+Shift+I)'
-const styleBkImgHIde = `
+const captionKey = 'Hide/Show Img(Ctrl+Shift+I)'
+
+class Himg {
+  state
+  stateOn
+  stateOff
+  constructor(on) {
+    this.stateOn = new StateOn(this)
+    this.stateOff = new StateOff(this)
+    this.setState(on)
+  }
+
+  setState(on) {
+    this.state = on ? this.stateOn : this.stateOff
+  }
+
+  switchMode() {
+    this.state.switchMode()
+  }
+}
+
+class StateOn {
+  hImg
+  constructor(hImg) {
+    this.hImg = hImg
+  }
+  switchMode() {
+    const content = `
 * { background-image: url("data:,") !important; }
 img,video{ visibility: hidden !important; }
 `
-
-const hide = () => {
-  regMenu(true)
-  GM_addStyle(styleBkImgHIde)
-}
-
-const show = () => {
-  regMenu(false)
-  Array.from(document.querySelectorAll('style'))
-    .filter((e) => e.id.startsWith('VMst0.'))
-    .forEach((e) => e.remove())
-}
-
-const regMenu = (isShow) => {
-  GM_unregisterMenuCommand(isShow ? captionHideKey : captionShowKey)
-  GM_registerMenuCommand(isShow ? captionShowKey : captionHideKey, () => {
-    GM_setValue(HI_STATUS, !isShow)
-  })
-}
-
-const main = () => {
-  if (GM_getValue(HI_STATUS, undefined) === undefined) {
-    GM_setValue(HI_STATUS, false)
+    const ele = GM_addElement(document.head, 'style', content)
+    ele.setAttribute('data-id', 'vm-hide-img')
   }
-  if (GM_getValue(HI_STATUS)) {
-    hide()
-  } else {
-    show()
+}
+
+class StateOff {
+  hImg
+  constructor(hImg) {
+    this.hImg = hImg
   }
+  switchMode() {
+    document.querySelectorAll("style[data-id='vm-hide-img']").forEach((e) => e.remove())
+  }
+}
+
+const updateVal = () => {
+  GM_setValue(HI_STATUS, !GM_getValue(HI_STATUS))
 }
 
 GM_addValueChangeListener(HI_STATUS, (name, oldValue, newValue, remote) => {
-  if (oldValue && !newValue) {
-    show()
-  } else if (!oldValue && newValue) {
-    hide()
-  }
+  hImg.setState(newValue)
+  hImg.switchMode()
 })
+
+GM_registerMenuCommand(captionKey, updateVal)
 
 register('c-s-i', () => {
-  GM_setValue(HI_STATUS, !GM_getValue(HI_STATUS))
+  updateVal()
 })
 
-main()
+hImg = new Himg(GM_getValue(HI_STATUS, undefined))
+hImg.switchMode()
